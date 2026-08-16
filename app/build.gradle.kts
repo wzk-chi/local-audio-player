8f6b8a8eb90bbce629eac0d6d5f1032aab101569
wzk <wzk1335@163.com>
2026-08-16 20:39:13 +0800

    build: separate debug application id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
