fc7c38b55a56a9a94e78611b4a64659a6f567e59
wzkw <wzk1335@163.com>
2026-08-30 23:57:41 +0800

    release: 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.localaudio.player"
         minSdk = 27
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
     }
 
     signingConfigs {
